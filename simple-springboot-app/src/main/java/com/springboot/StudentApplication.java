ea9545252f178fbd969b66bf1893bb3895d59689
Andrey.Klyuev <Andrey.Klyuev@jetbrains.com>
2020-09-27 19:42:49 +0300

    adasdasd

--- simple-springboot-app/src/main/java/com/springboot/StudentApplication.java
+++ simple-springboot-app/src/main/java/com/springboot/StudentApplication.java
@@ -3,11 +3,27 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 @SpringBootApplication
 public class StudentApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
-		11saljl
+		final HashMap<Integer, String> count = new HashMap<>();
+		final List<String> strings = Arrays.asList(
+						"sandkadnaks dnaskdn daldsnaksj daldsnaksj",
+						"sandkadnaks daldsnaksj",
+						"sandkadnaks dnaskdn daldsnaksj",
+						"sandkadnaks dnaskdn daldsnaksj daldsnaksj daldsnaksj");
+		int key = 0;
+		count.compute(
+						key, (x, s) -> {return s;}
+		);
+
 	}
 }
